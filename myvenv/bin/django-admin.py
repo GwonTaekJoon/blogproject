13da5e1992692e1aca25e26c73029398070f5644
권택준 <kyuzu123123@likelion.org>
2019-06-19 09:45:28 +0900

    firstcommit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Downloads/blogproject-d7df1eac0f8a9ec480a5b1bb8ff3157f8b9ead52/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
